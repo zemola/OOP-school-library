bc625452ca96ba39a6b34da15cf05d893f263621
unknown <azeemolatunji@gmail.com>
2022-03-01 19:47:09 +0100

    Create teacher class and inherit informations from person

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initializing(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initializing(classroom, age, name, parent_permission)
+  def initialize(classroom, age, name, parent_permission)
     super(age, name, parent_permission)
     @classroom = classroom
   end
