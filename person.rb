0ce6b2c2003542d27b521198cef4687ebd2133bd
unknown <azeemolatunji@gmail.com>
2022-03-02 11:26:03 +0100

    set an instance of corrector in person

--- person.rb
+++ person.rb
@@ -1,3 +1,4 @@
+require './corrector'
 class Person
   attr_reader :id
   attr_accessor :name, :age
@@ -7,6 +8,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   private
@@ -19,5 +21,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(name)
   end
 end
