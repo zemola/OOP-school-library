193c9c494ec8c22b93779e749e779d04f477d59e
unknown <azeemolatunji@gmail.com>
2022-03-02 16:56:46 +0100

    create book class and link with rentals

---
+++ book.rb
@@ -0,0 +1,17 @@
+require './rental'
+
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initilizer(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+    
+  end
+  
+
+end
