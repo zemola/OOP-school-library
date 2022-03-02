7e918b539ecb7a729cd8f8f545002409802c13c8
unknown <azeemolatunji@gmail.com>
2022-03-02 23:00:39 +0100

    made changes as requested by reviwer

--- classroom.rb
+++ classroom.rb
@@ -1,15 +1,16 @@
 require './student'
+require './person'
+
 class Classroom
   attr_accessor :label
   attr_reader :students
 
   def initialize(label)
     @label = label
-    @student = []
   end
 
   def add_students(student)
-    @students.push(@student)
+    @students.push(student)
     student.classroom = self
   end
 end
