feeaa7885022fab56e498f324846ab0bee907f9f
unknown <azeemolatunji@gmail.com>
2022-03-03 21:30:07 +0100

    create meanu method

--- main.rb
+++ main.rb
@@ -22,6 +22,22 @@ def start
     menu
     option = get.chomp
     break if option == '7'
+
+    option_output option
   end
+  puts 'Thank you for using our system!'
+end
+
+def menu
+  puts
+  puts 'Please choose an option by entering a number between 1 to 7: '
+  puts ' 1 - List all books'
+  puts ' 2 - List all people'
+  puts ' 3 - Create a person'
+  puts ' 4 - Create a book'
+  puts ' 5 - Create a rental'
+  puts ' 6 - List all rental for given person id'
+  puts ' 7 - Exit'
+  puts '********************************************************'
 end
 
