2b5657bf5f24ba0a3edeffb8bef6e691c812665a
unknown <azeemolatunji@gmail.com>
2022-03-09 15:48:00 +0100

    Added methods to load book,person and rentals

--- data_store.rb
+++ data_store.rb
@@ -24,4 +24,67 @@ module DataStore
       []
     end
   end
+  def load_books
+    file = 'books.json'
+    if File.exist? file
+      JSON.parse(File.read(file)).map do |book|
+        Book.new(book['title'], book['author'])
+      end
+    else
+      []
+    end
+  end
+
+  def get_person_by_id(id)
+    @people.each { |person| return person if person.id == id }
+  end
+
+  def get_book_by_title(title)
+    @books.each { |book| return book if book.title == title }
+  end
+
+  def load_rentals
+    file = 'rentals.json'
+    if File.exist? file
+      JSON.parse(File.read(file)).map do |rental|
+        date = rental['date']
+        person = get_person_by_id(rental['person'])
+        book = get_book_by_title(rental['book'])
+        Rental.new(date, book, person)
+      end
+    else
+      []
+    end
+  end
+
+  def save_people
+    data = []
+    @people.each do |people|
+      if people.instance_of?(Teacher)
+        data.push({ key: 'teacher', id: people.id, age: people.age, name: people.name,
+                    specialization: people.specialization })
+      else
+        data.push({ key: 'student', id: people.id, age: people.age, name: people.name,
+                    permission: people.parent_permission, classroom: people.classroom })
+      end
+    end
+    open('person.json', 'w') { |f| f.write JSON.generate(data) }
+  end
+
+  def save_rental
+    data = []
+    @rentals.each do |rental|
+      data.push({ date: rental.date, person: rental.person.id, book: rental.book.title })
+    end
+    open('rentals.json', 'w') { |f| f << JSON.generate(data) }
+  end
+
+  def save_books
+    data = []
+    @books.each do |book|
+      data.push({ title: book.title, author: book.author })
+    end
+    open('books.json', 'w') { |f| f << JSON.generate(data) }
+  end
 end
+
